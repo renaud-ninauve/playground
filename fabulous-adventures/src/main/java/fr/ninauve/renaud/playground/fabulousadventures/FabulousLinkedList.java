484c22884da284456903289c854d0ac1e82a6f26
renaud-ninauve <renaud.ninauve.92@gmail.com>
2026-04-30 17:42:08 +0200

    reverse

--- fabulous-adventures/src/main/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousLinkedList.java
+++ fabulous-adventures/src/main/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousLinkedList.java
@@ -8,12 +8,29 @@ public sealed interface FabulousLinkedList<T> {
     }
 
     FabulousLinkedList<T> push(T value);
+    FabulousLinkedList<T> reverse();
     Stream<T> stream();
 
     record NotEmpty<T>(T value, FabulousLinkedList<T> tail) implements FabulousLinkedList<T> {
         public FabulousLinkedList<T> push(T value) {
             return new NotEmpty<>(value, this);
         }
+
+        @Override
+        public FabulousLinkedList<T> reverse() {
+            FabulousLinkedList<T> reversed = new Empty<>();
+            FabulousLinkedList<T> current = this;
+            while (!(current instanceof FabulousLinkedList.Empty<T>)) {
+                if (current instanceof FabulousLinkedList.NotEmpty<T>(
+                        T currentValue, FabulousLinkedList<T> currentTail
+                )) {
+                    reversed = reversed.push(currentValue);
+                    current = currentTail;
+                }
+            }
+            return reversed;
+        }
+
         public Stream<T> stream() {
             Stream.Builder<T> stream = Stream.builder();
             FabulousLinkedList<T> current = this;
@@ -33,6 +50,11 @@ public sealed interface FabulousLinkedList<T> {
         }
         public Stream<T> stream() {
             return Stream.empty();
+        }
+
+        @Override
+        public FabulousLinkedList<T> reverse() {
+            return this;
         }
     }
 }
--- fabulous-adventures/src/test/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousLinkedListTest.java
+++ fabulous-adventures/src/test/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousLinkedListTest.java
@@ -9,14 +9,28 @@ class FabulousLinkedListTest {
 
     @Test
     void should_stream() {
-        FabulousLinkedList<Integer> list = FabulousLinkedList
+        FabulousLinkedList<Integer> actual = FabulousLinkedList
                 .of(1)
                 .push(2)
                 .push(3)
                 .push(4)
                 .push(5);
 
-        assertThat(list.stream())
+        assertThat(actual.stream())
                 .containsExactly(5, 4, 3, 2, 1);
+    }
+
+    @Test
+    void should_reverse() {
+        FabulousLinkedList<Integer> list = FabulousLinkedList
+                .of(1)
+                .push(2)
+                .push(3)
+                .push(4)
+                .push(5);
+        FabulousLinkedList<Integer> actual = list.reverse();
+
+        assertThat(actual.stream())
+                .containsExactly(1, 2, 3, 4, 5);
     }
 }
