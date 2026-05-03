f7b9fc6b809a6dc4200462d9a4d8a4d81f985c32
renaud-ninauve <renaud.ninauve.92@gmail.com>
2026-05-03 17:33:30 +0200

    hughes

--- fabulous-adventures/src/main/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousStack.java
+++ fabulous-adventures/src/main/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousStack.java
@@ -1,5 +1,6 @@
 package fr.ninauve.renaud.playground.fabulousadventures;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public sealed interface FabulousStack<T> extends Streamable<T> {
@@ -110,6 +111,44 @@ public sealed interface FabulousStack<T> extends Streamable<T> {
                 }
             }
             return stream.build();
+        }
+    }
+
+    static <T> HughesList<T> newHughes() {
+        return new HughesList<>(Function.identity());
+    }
+    static <T> HughesList<T> newHughes(T item) {
+        return new HughesList<>(stack -> stack.push(item));
+    }
+
+    record HughesList<T>(Function<FabulousStack<T>, FabulousStack<T>> h) {
+
+        public FabulousStack<T> toStack() {
+            return h.apply(newStack());
+        }
+        private static <T> HughesList<T> fromStack(FabulousStack<T> from) {
+            return new HughesList<>(from::concatenate);
+        }
+        public HughesList<T> push(T value) {
+            return concatenate(newHughes(value), this);
+        }
+        public T peek() {
+            return toStack().peek();
+        }
+        public HughesList<T> pop() {
+            return fromStack(toStack().pop());
+        }
+        public boolean isEmpty() {
+            return toStack().isEmpty();
+        }
+        public static <T> HughesList<T> concatenate(HughesList<T> left, HughesList<T> right) {
+            return new HughesList<>(s -> left.h.apply(right.h.apply(s)));
+        }
+        public HughesList<T> append(T a) {
+            return concatenate(this, newHughes(a));
+        }
+        public Stream<T> stream() {
+            return toStack().stream();
         }
     }
 }
--- fabulous-adventures/src/test/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousStackTest.java
+++ fabulous-adventures/src/test/java/fr/ninauve/renaud/playground/fabulousadventures/FabulousStackTest.java
@@ -1,8 +1,10 @@
 package fr.ninauve.renaud.playground.fabulousadventures;
 
+import fr.ninauve.renaud.playground.fabulousadventures.FabulousStack.HughesList;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static fr.ninauve.renaud.playground.fabulousadventures.FabulousStack.newHughes;
 import static fr.ninauve.renaud.playground.fabulousadventures.FabulousStack.newStack;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,7 +41,7 @@ class FabulousStackTest {
         @Test
         void reverseOnto_should_return_ontoTail() {
             FabulousStack<String> stack = newStack();
-            FabulousStack<String> ontoTail = NotEmpty.stackOf("A", "B", "C");
+            FabulousStack<String> ontoTail = stackOf("A", "B", "C");
             FabulousStack<String> actual = stack.reverseOnto(ontoTail);
             assertThat(actual).isEqualTo(ontoTail);
         }
@@ -47,7 +49,7 @@ class FabulousStackTest {
         @Test
         void concatenate_should_return_other() {
             FabulousStack<String> stack = newStack();
-            FabulousStack<String> other = NotEmpty.stackOf("A", "B", "C");
+            FabulousStack<String> other = stackOf("A", "B", "C");
             FabulousStack<String> actual = stack.concatenate(other);
             assertThat(actual).isEqualTo(other);
         }
@@ -56,7 +58,7 @@ class FabulousStackTest {
         void append_should_return_value() {
             FabulousStack<String> stack = newStack();
             FabulousStack<String> actual = stack.append("A");
-            assertThat(actual).isEqualTo(NotEmpty.stackOf("A"));
+            assertThat(actual).isEqualTo(stackOf("A"));
         }
     }
 
@@ -97,7 +99,7 @@ class FabulousStackTest {
         @Test
         void should_reverseOnto() {
             FabulousStack<String> stack = stackOf("A", "B", "C");
-            FabulousStack<String> ontoTail = NotEmpty.stackOf("D", "E", "F");
+            FabulousStack<String> ontoTail = stackOf("D", "E", "F");
             FabulousStack<String> actual = stack.reverseOnto(ontoTail);
             assertThat(actual).isEqualTo(stackOf("D", "E", "F", "C", "B", "A"));
         }
@@ -112,7 +114,7 @@ class FabulousStackTest {
         @Test
         void should_concatenate() {
             FabulousStack<String> stack = stackOf("A", "B", "C");
-            FabulousStack<String> other = NotEmpty.stackOf("D", "E", "F");
+            FabulousStack<String> other = stackOf("D", "E", "F");
             FabulousStack<String> actual = stack.concatenate(other);
             assertThat(actual).isEqualTo(stackOf("D", "E", "F", "A", "B", "C"));
         }
@@ -123,9 +125,65 @@ class FabulousStackTest {
             FabulousStack<String> actual = stack.append("D");
             assertThat(actual).isEqualTo(stackOf("D", "A", "B", "C"));
         }
+    }
 
-        private static FabulousStack<String> stackOf(String... values) {
-            FabulousStack<String> stack = newStack();
+    private static FabulousStack<String> stackOf(String... values) {
+        FabulousStack<String> stack = newStack();
+        for(String value: values) {
+            stack = stack.push(value);
+        }
+        return stack;
+    }
+
+    @Nested
+    class Hughes {
+        @Test
+        void isEmpty_should_return_false() {
+            HughesList<String> stack = hughesOf("A");
+            assertThat(stack.isEmpty()).isFalse();
+        }
+        @Test
+        void stream_should_list_elements() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            assertThat(stack.stream()).containsExactly("C", "B", "A");
+        }
+
+        @Test
+        void peek_should_return_last() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            assertThat(stack.peek()).isEqualTo("C");
+        }
+
+        @Test
+        void pop_should_return_tail() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            assertThat(stack.pop().stream()).containsExactly("B", "A");
+        }
+
+        @Test
+        void should_concatenate_empty() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            HughesList<String> actual = HughesList.concatenate(stack, newHughes());
+            assertThat(actual.toStack()).isEqualTo(stackOf("A", "B", "C"));
+        }
+
+        @Test
+        void should_concatenate() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            HughesList<String> other = hughesOf("D", "E", "F");
+            HughesList<String> actual = HughesList.concatenate(stack, other);
+            assertThat(actual.toStack()).isEqualTo(stackOf("D", "E", "F", "A", "B", "C"));
+        }
+
+        @Test
+        void should_append() {
+            HughesList<String> stack = hughesOf("A", "B", "C");
+            HughesList<String> actual = stack.append("D");
+            assertThat(actual.toStack()).isEqualTo(stackOf("D", "A", "B", "C"));
+        }
+
+        private static HughesList<String> hughesOf(String... values) {
+            HughesList<String> stack = newHughes();
             for(String value: values) {
                 stack = stack.push(value);
             }
